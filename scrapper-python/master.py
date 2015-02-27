7d070ab014efaac21b511f5120dd8585aed57c0b
bbudhathoki <binay.b@gmail.com>
2015-02-27 16:12:54 +0545

    submit jobs to each of the threads

--- scrapper-python/master.py
+++ scrapper-python/master.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import os
+import os, threading
 
 def create_dir(dirname):
 	if not os.path.exists(dirname):
@@ -13,8 +13,19 @@ def create_dir(dirname):
 def log(msg):
 	print msg
 
+def fetch_home_content(url_chunk, i):
+	for url in url_chunk:
+		print url + " -- " + str(i)
+		
+	#Maybe insert something in the queue
+	
 if(__name__ == "__main__"):
 	MAX_THREAD = 5
+	
+	home_ongoing_queue = Queue()
+	facebook_ongoing_queue = Queue()
+	linkedin_ongoing_queue = Queue()
+	twitter_ongoing_queue = Queue()
 	
 	#1. Initialize the required folder structure
 	log("Creating Directory Structure.")
@@ -36,11 +47,27 @@ if(__name__ == "__main__"):
 		
 	#3. Distrubute the urls for each of the threads
 	log('Distributing Thread Jobs.')
-	url_distribution = []
+	chunk_holder = []
 	for i in range(MAX_THREAD):
-		url_distribution.append([])
+		chunk_holder.append([])
 	
 	url_count = 0
 	for url in url_list:
-		url_distribution[url_count % MAX_THREAD].append(url)
+		chunk_holder[url_count % MAX_THREAD].append(url)
 		url_count = url_count + 1
+		
+	#4. Submit the jobs to the subsequent threads
+	my_threads = []
+	thread_count = 0
+	for url_chunk in chunk_holder:
+		thread_fetch_home_content = threading.Thread(target=fetch_home_content, args=(url_chunk, thread_count,))
+		thread_fetch_home_content.start()
+		my_threads.append(thread_fetch_home_content)
+		thread_count = thread_count + 1
+		
+	#5. Syncronize the threads
+	for thread in my_threads:
+		thread.join()
+		
+	
+	
