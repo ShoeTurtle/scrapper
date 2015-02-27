9fe129232fee195af3f773e4809d30d1a5774945
bbudhathoki <binay.b@gmail.com>
2015-02-27 22:20:40 +0545

    write response to file

--- scrapper-python/utilities.py
+++ scrapper-python/utilities.py
@@ -1,4 +1,4 @@
-import os
+import os, re
 
 #Print log message to the console
 def log(msg):
@@ -12,3 +12,18 @@ def create_dir(dirname):
 		except:
 			log('Unable to create directory')
 			sys.exit(0)
+			
+#Get file name from the url
+def get_file_name(url):
+	url = re.sub(r'https?:\/\/(www\.)?', '', url)
+	return url.split('.')[0]
+			
+#Write the web content response to a file
+def write_reponse(response, file_name, scrapper_type):
+	dir_path = os.path.abspath('../') + '/resource/' + scrapper_type
+	try:
+		fobj = open(os.path.join(dir_path, file_name), 'w+')
+		fobj.write(response.read())
+		fobj.close()
+	except Exception, e:
+		log(e)
