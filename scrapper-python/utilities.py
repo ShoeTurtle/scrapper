ae049b5204e72e8162107ed4a0a55581774b57e8
bbudhathoki <binay.b@gmail.com>
2015-02-28 23:29:01 +0545

    add facebook fan page url extract

--- scrapper-python/utilities.py
+++ scrapper-python/utilities.py
@@ -1,4 +1,5 @@
-import os, re
+import os, re, urllib2
+from xml.dom import minidom
 
 #Print log message to the console
 def log(msg):
@@ -27,3 +28,31 @@ def write_reponse(response, file_name, scrapper_type):
 		fobj.close()
 	except Exception, e:
 		log(e)
+		
+#Extract Facebook fan page link
+def get_facebook_link(soup):
+	facebook_url = None
+
+	if not facebook_url and soup.find('a', href = re.compile(r'.*facebook.*')):
+		facebook_url = soup.find('a', href = re.compile(r'.*facebook.*'))['href']
+	if not facebook_url and soup.find('div', {"class" : "fb-like"}):
+		facebook_url = soup.find('div', {"class" : "fb-like"})['data-href']
+	if not facebook_url and soup.find('div', {"class" : "fb-like-box"}):
+		facebook_url = soup.find('div', {"class" : "fb-like-box"})["data-href"]
+		
+	return facebook_url if facebook_url else ''
+	
+#Read URL From the Resource file.
+def get_url_list():
+	log("Reading URL.")
+	url_path = os.path.abspath('../') + "/resource/urls.txt"
+	url_list = []
+
+	try:
+		fobj = open(url_path)
+		url_list = [url.strip() for url in fobj.readlines()]
+		fobj.close()
+	except:
+		log("Unable to Open File!!")
+		
+	return url_list
