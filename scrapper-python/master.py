166c688c9b1bb5e4c98c66b78553006dde499a64
bbudhathoki <binay.b@gmail.com>
2015-02-28 23:27:49 +0545

    rearrange function calls

--- scrapper-python/master.py
+++ scrapper-python/master.py
@@ -2,6 +2,7 @@
 
 from utilities import *
 from home_scraper import *
+from alexa import *
 
 if(__name__ == "__main__"):
 	#1. Initialize the required folder structure
@@ -10,13 +11,10 @@ if(__name__ == "__main__"):
 	for folder_name in folder_list:
 		create_dir("../resource/" + folder_name)
 		
-	#2. Initialize Home Scrapper
-	init_home_scapper()
-	
-	#3. Initialize Facebook Scrapper
-	
-	#4. Initialize Twitter Scrapper
-	
-	#5. Initialize Linkedin Scrapper
+	#2. Initialize Home Scrapper; Write the Home stats to the database
+	# init_home_scapper()
+	# parse_home()
+	fetch_alexa_rating()
+	# get_page_rank()
 	
 	log('\n**SCRAPING COMPLETE**')
