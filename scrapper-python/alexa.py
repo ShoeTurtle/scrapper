80d0198eccad8afc18a220b73c43cdf229a0cdee
bbudhathoki <binay.b@gmail.com>
2015-03-01 09:07:33 +0545

    using threads to fetch the alexa ratings

--- scrapper-python/alexa.py
+++ scrapper-python/alexa.py
@@ -1,14 +1,33 @@
-import urllib, sys
+import urllib, sys, threading
 from utilities import *
 from xml.dom import minidom
+from Queue import Queue
+
+alexa_url_queue = Queue(maxsize = 0)
+ALEXA_THREAD_COUNT = 5
+
+#The Worker Funtion for the Alexa Thread Block
+def alexa_worker():
+	while True:
+		url = alexa_url_queue.get()
+		print alexa_rating(url)
+		alexa_url_queue.task_done()
 
 #Retrieve the alexa rating and store it into the database
 def fetch_alexa_rating():
 	url_list = get_url_list()
 	
-	for each_url in url_list:
-		print alexa_rating(each_url)
+	for i in range(ALEXA_THREAD_COUNT):
+		alexa_thread = threading.Thread(target=alexa_worker)
+		alexa_thread.daemon = True
+		alexa_thread.start()
 		
+	#Queue up all the urls
+	for each_url in url_list:
+		alexa_url_queue.put(each_url)
+		
+	alexa_url_queue.join()
+	
 #Fetch the Alexa Rating for each for each of the url
 def alexa_rating(url):
 	xml_block = urllib.urlopen("http://data.alexa.com/data?cli=10&dat=s&url="+ url).read()
