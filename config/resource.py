b18f2cd1d01cd91afae3ff320a9951dea6a4b1ae
bbudhathoki <binay.b@gmail.com>
2015-03-02 21:03:46 +0545

    add resource config file

---
+++ config/resource.py
@@ -0,0 +1,21 @@
+#Example config Structure
+# truck = dict(
+#     color = 'blue',
+#     brand = 'ford',
+# )
+# city = 'new york'
+# cabriolet = dict(
+#     color = 'black',
+#     engine = dict(
+#         cylinders = 8,
+#         placement = 'mid',
+#     ),
+#     doors = 2,
+# )
+
+server_host = 127.0.0.1
+#MongoDB Configuration
+db_config = dict (
+	host_ip = 'localhost',
+	host_port = '27017'	
+)
